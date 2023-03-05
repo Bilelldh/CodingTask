ef75807ffbfbc8e57789b0d0c891e00c375d278d
Bilelldh <ldhbilel@gmail.com>
2023-03-05 11:20:55 +0100

    edited Authgard

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,49 @@
-import {createRouter , createWebHashHistory} from 'vue-router'
-import Login from '../pages/login.vue';
-import Detail from '../pages/detail.vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import Login from "../pages/login.vue";
+import Detail from "../pages/detail.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'login',
-        component: Login,
-
-    } ,
-    {
-        path: '/detail',
-        name: 'Detail',
-        component: Detail,
-        meta: {
-            needsAuth: true
-        }
-    } ,
-
-]
-
+  {
+    path: "/",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/detail",
+    name: "Detail",
+    component: Detail,
+    meta: {
+      needsAuth: true,
+    },
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(process.env.BASE_URL),
-    routes
-})
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes,
+});
 
-//Auth Guards 
+//Auth Guards
 router.beforeEach((to, from, next) => {
+  // get the token from local storage
+  const token = localStorage.getItem("isLogged");
 
-    // get the token from local storage
-    const token = localStorage.getItem('isLogged');
+  if (to.meta.needsAuth) {
+    // if the page needs authentication
 
-    if (to.meta.needsAuth) { // if the page needs authentication
-       
-        if (token) {
-            next() // you can go to that page
-        } else {
-            next('/') // you can't go to that page
-        }
-    } else {
-        // you can't go to login page if you're already logged in
-        if(token){
-            next("/Detail") ;  
-        }
-        next();
+    if (!token) {
+      // you can't go to that page
+      next("/");
     }
-})
+  } else {
+    // you can't go to login page if you're already logged in
+    if (token) {
+      next("/Detail");
+    }
+  }
 
-export default router
+  // you can go to that page
+  next();
+});
+
+export default router;
